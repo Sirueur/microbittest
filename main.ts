bbbda769c4425f241ded4981397a378648633db8
Sirueur <123979452+Sirueur@users.noreply.github.com>
2023-02-10 12:24:44 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,61 @@
-
+let secouer = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . # .
+        . . . # .
+        `)
+    if (secouer == 3) {
+        basic.showLeds(`
+            . # # . .
+            # . # . .
+            . . # . .
+            . . # . .
+            # # # # #
+            `)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    if (secouer == 3) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # # # .
+        `)
+    if (secouer == 3) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    secouer = 3
+})
+basic.forever(function () {
+	
+})
